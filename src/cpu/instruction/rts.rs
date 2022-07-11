5fd4bd0201fedc708bec4e59b6190f3b5191975d
Krister Jansson <krustur@gmail.com>
2022-07-11 22:19:07 +0200

    rts instruction tests

--- src/cpu/instruction/rts.rs
+++ src/cpu/instruction/rts.rs
@@ -39,179 +39,88 @@ pub fn get_disassembly<'a>(
 
 #[cfg(test)]
 mod tests {
-    use crate::cpu::instruction::GetDisassemblyResult;
+    use crate::{
+        cpu::instruction::GetDisassemblyResult,
+        mem::rammemory::RamMemory,
+        register::{
+            STATUS_REGISTER_MASK_CARRY, STATUS_REGISTER_MASK_EXTEND, STATUS_REGISTER_MASK_NEGATIVE,
+            STATUS_REGISTER_MASK_OVERFLOW, STATUS_REGISTER_MASK_ZERO,
+        },
+    };
 
-    // // byte
+    #[test]
+    fn rts_dont_set_any_sr() {
+        // arrange
+        let code = [0x4e, 0x75].to_vec(); // RTS
+        let mem_range = RamMemory::from_bytes(0x00F80000, [0x00, 0xc0, 0x12, 0x48].to_vec());
+        let mut mem_ranges = Vec::new();
+        mem_ranges.push(mem_range);
 
-    // #[test]
-    // fn step_bsr_byte() {
-    //     // arrange
-    //     let code = [0x61, 0x02].to_vec(); // BSR.B $02
-    //     let mut cpu = crate::instr_test_setup(code, None);
-    //     cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000); //STATUS_REGISTER_MASK_CARRY;
+        let mut cpu = crate::instr_test_setup(code, Some(mem_ranges));
+        cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000);
+        cpu.register.set_a_reg_long(7, 0x00F80000);
+        // act assert - debug
+        let debug_result = cpu.get_next_disassembly();
+        assert_eq!(
+            GetDisassemblyResult::from_address_and_address_next(
+                0xC00000,
+                0xC00002,
+                String::from("RTS"),
+                String::from("")
+            ),
+            debug_result
+        );
+        // act
+        cpu.execute_next_instruction();
+        // assert
+        assert_eq!(0xC01248, cpu.register.reg_pc.get_address());
+        assert_eq!(0x00F80004, cpu.register.get_a_reg_long(7));
 
-    //     println!("sp: ${:08X}", cpu.register.get_a_reg_long(7));
+        assert_eq!(false, cpu.register.reg_sr.is_sr_carry_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_overflow_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_zero_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_negative_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_extend_set());
+    }
 
-    //     // act assert - debug
-    //     let debug_result = cpu.get_next_disassembly();
-    //     assert_eq!(
-    //         GetDisassemblyResult::from_address_and_address_next(
-    //             0xC00000,
-    //             0xC00002,
-    //             String::from("BSR.B"),
-    //             String::from("$02 [$00C00004]")
-    //         ),
-    //         debug_result
-    //     );
-    //     // act
-    //     cpu.execute_next_instruction();
-    //     // assert
-    //     assert_eq!(0xC00004, cpu.register.reg_pc.get_address());
-    //     assert_eq!(0x10003fc, cpu.register.get_a_reg_long(7));
-    //     assert_eq!(0xC00002, cpu.memory.get_long(0x10003fc));
-    // }
+    #[test]
+    fn rts_dont_clear_any_sr() {
+        // arrange
+        let code = [0x4e, 0x75].to_vec(); // RTS
+        let mem_range = RamMemory::from_bytes(0x00F80000, [0x00, 0xc0, 0x12, 0x48].to_vec());
+        let mut mem_ranges = Vec::new();
+        mem_ranges.push(mem_range);
 
-    // #[test]
-    // fn step_bsr_byte_negative() {
-    //     // arrange
-    //     let code = [0x61, 0xfc].to_vec(); // BSR.B $FC
-    //     let mut cpu = crate::instr_test_setup(code, None);
-    //     cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000); //STATUS_REGISTER_MASK_CARRY;
+        let mut cpu = crate::instr_test_setup(code, Some(mem_ranges));
+        cpu.register.reg_sr.set_sr_reg_flags_abcde(
+            STATUS_REGISTER_MASK_CARRY
+                | STATUS_REGISTER_MASK_OVERFLOW
+                | STATUS_REGISTER_MASK_ZERO
+                | STATUS_REGISTER_MASK_NEGATIVE
+                | STATUS_REGISTER_MASK_EXTEND,
+        );
+        cpu.register.set_a_reg_long(7, 0x00F80000);
+        // act assert - debug
+        let debug_result = cpu.get_next_disassembly();
+        assert_eq!(
+            GetDisassemblyResult::from_address_and_address_next(
+                0xC00000,
+                0xC00002,
+                String::from("RTS"),
+                String::from("")
+            ),
+            debug_result
+        );
+        // act
+        cpu.execute_next_instruction();
+        // assert
+        assert_eq!(0xC01248, cpu.register.reg_pc.get_address());
+        assert_eq!(0x00F80004, cpu.register.get_a_reg_long(7));
 
-    //     println!("sp: ${:08X}", cpu.register.get_a_reg_long(7));
-
-    //     // act assert - debug
-    //     let debug_result = cpu.get_next_disassembly();
-    //     assert_eq!(
-    //         GetDisassemblyResult::from_address_and_address_next(
-    //             0xC00000,
-    //             0xC00002,
-    //             String::from("BSR.B"),
-    //             String::from("$FC [$00BFFFFE]")
-    //         ),
-    //         debug_result
-    //     );
-    //     // act
-    //     cpu.execute_next_instruction();
-    //     // assert
-    //     assert_eq!(0xBFFFFE, cpu.register.reg_pc.get_address());
-    //     assert_eq!(0x10003fc, cpu.register.get_a_reg_long(7));
-    //     assert_eq!(0xC00002, cpu.memory.get_long(0x10003fc));
-    // }
-
-    // // word
-
-    // #[test]
-    // fn step_bsr_word() {
-    //     // arrange
-    //     let code = [0x61, 0x00, 0x00, 0x04].to_vec(); // BSR.W $0004
-    //     let mut cpu = crate::instr_test_setup(code, None);
-    //     cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000); //STATUS_REGISTER_MASK_CARRY;
-
-    //     println!("sp: ${:08X}", cpu.register.get_a_reg_long(7));
-
-    //     // act assert - debug
-    //     let debug_result = cpu.get_next_disassembly();
-    //     assert_eq!(
-    //         GetDisassemblyResult::from_address_and_address_next(
-    //             0xC00000,
-    //             0xC00004,
-    //             String::from("BSR.W"),
-    //             String::from("$0004 [$00C00006]")
-    //         ),
-    //         debug_result
-    //     );
-    //     // act
-    //     cpu.execute_next_instruction();
-    //     // assert
-    //     assert_eq!(0xC00006, cpu.register.reg_pc.get_address());
-    //     assert_eq!(0x10003fc, cpu.register.get_a_reg_long(7));
-    //     assert_eq!(0xC00004, cpu.memory.get_long(0x10003fc));
-    // }
-
-    // #[test]
-    // fn step_bsr_word_negative() {
-    //     // arrange
-    //     let code = [0x61, 0x00, 0xff, 0xfc].to_vec(); // BSR.W $FFFC
-    //     let mut cpu = crate::instr_test_setup(code, None);
-    //     cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000); //STATUS_REGISTER_MASK_CARRY;
-
-    //     println!("sp: ${:08X}", cpu.register.get_a_reg_long(7));
-
-    //     // act assert - debug
-    //     let debug_result = cpu.get_next_disassembly();
-    //     assert_eq!(
-    //         GetDisassemblyResult::from_address_and_address_next(
-    //             0xC00000,
-    //             0xC00004,
-    //             String::from("BSR.W"),
-    //             String::from("$FFFC [$00BFFFFE]")
-    //         ),
-    //         debug_result
-    //     );
-    //     // act
-    //     cpu.execute_next_instruction();
-    //     // assert
-    //     assert_eq!(0xBFFFFE, cpu.register.reg_pc.get_address());
-    //     assert_eq!(0x10003fc, cpu.register.get_a_reg_long(7));
-    //     assert_eq!(0xC00004, cpu.memory.get_long(0x10003fc));
-    // }
-
-    // // long
-
-    // #[test]
-    // fn step_bsr_long() {
-    //     // arrange
-    //     let code = [0x61, 0xff, 0x00, 0x00, 0x00, 0x06].to_vec(); // BSR.L $00000006
-    //     let mut cpu = crate::instr_test_setup(code, None);
-    //     cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000); //STATUS_REGISTER_MASK_CARRY;
-
-    //     println!("sp: ${:08X}", cpu.register.get_a_reg_long(7));
-
-    //     // act assert - debug
-    //     let debug_result = cpu.get_next_disassembly();
-    //     assert_eq!(
-    //         GetDisassemblyResult::from_address_and_address_next(
-    //             0xC00000,
-    //             0xC00006,
-    //             String::from("BSR.L"),
-    //             String::from("$00000006 [$00C00008]")
-    //         ),
-    //         debug_result
-    //     );
-    //     // act
-    //     cpu.execute_next_instruction();
-    //     // assert
-    //     assert_eq!(0xC00008, cpu.register.reg_pc.get_address());
-    //     assert_eq!(0x10003fc, cpu.register.get_a_reg_long(7));
-    //     assert_eq!(0xC00006, cpu.memory.get_long(0x10003fc));
-    // }
-
-    // #[test]
-    // fn step_bsr_long_negative() {
-    //     // arrange
-    //     let code = [0x61, 0xff, 0xff, 0xff, 0xff, 0xfc].to_vec(); // BSR.L $FFFFFFFC
-    //     let mut cpu = crate::instr_test_setup(code, None);
-    //     cpu.register.reg_sr.set_sr_reg_flags_abcde(0x0000); //STATUS_REGISTER_MASK_CARRY;
-
-    //     println!("sp: ${:08X}", cpu.register.get_a_reg_long(7));
-
-    //     // act assert - debug
-    //     let debug_result = cpu.get_next_disassembly();
-    //     assert_eq!(
-    //         GetDisassemblyResult::from_address_and_address_next(
-    //             0xC00000,
-    //             0xC00006,
-    //             String::from("BSR.L"),
-    //             String::from("$FFFFFFFC [$00BFFFFE]")
-    //         ),
-    //         debug_result
-    //     );
-    //     // act
-    //     cpu.execute_next_instruction();
-    //     // assert
-    //     assert_eq!(0xBFFFFE, cpu.register.reg_pc.get_address());
-    //     assert_eq!(0x10003fc, cpu.register.get_a_reg_long(7));
-    //     assert_eq!(0xC00006, cpu.memory.get_long(0x10003fc));
-    // }
+        assert_eq!(true, cpu.register.reg_sr.is_sr_carry_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_overflow_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_zero_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_negative_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_extend_set());
+    }
 }
