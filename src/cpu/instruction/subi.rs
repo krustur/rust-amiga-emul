c111dcc39e86b3448759858e64b30e2f66b22e1e
Krister Jansson <krustur@gmail.com>
2022-07-20 23:32:51 +0200

    Fixed subi EA addressing bug, now reads immediate data before ea data correctly

--- src/cpu/instruction/subi.rs
+++ src/cpu/instruction/subi.rs
@@ -19,32 +19,60 @@ pub fn step<'a>(
     reg: &mut Register,
     mem: &mut Mem,
 ) -> Result<(), StepError> {
-    let ea_data =
-        pc.fetch_effective_addressing_data_from_bit_pos_3_and_reg_pos_0(reg, mem, |instr_word| {
-            Cpu::extract_size000110_from_bit_pos_6(instr_word)
-        })?;
-    let status_register_result = match ea_data.operation_size {
+    let instr_word = pc.fetch_next_word(mem);
+    let operation_size = Cpu::extract_size000110_from_bit_pos_6(instr_word)?;
+    let status_register_result = match operation_size {
         OperationSize::Byte => {
             pc.skip_byte();
-            let data = pc.fetch_next_byte(mem);
-            let ea_value = ea_data.get_value_byte(pc, reg, mem, false);
-            let add_result = Cpu::sub_bytes(data, ea_value);
-            ea_data.set_value_byte(pc, reg, mem, add_result.result, true);
-            add_result.status_register_result
+            let source = pc.fetch_next_byte(mem);
+
+            let ea_data = pc.get_effective_addressing_data_from_instr_word_bit_pos(
+                instr_word,
+                reg,
+                mem,
+                |instr_word| Ok(operation_size),
+                3,
+                0,
+            )?;
+            let dest = ea_data.get_value_byte(pc, reg, mem, true);
+
+            let result = Cpu::sub_bytes(source, dest);
+            ea_data.set_value_byte(pc, reg, mem, result.result, true);
+            result.status_register_result
         }
         OperationSize::Word => {
-            let data = pc.fetch_next_word(mem);
-            let ea_value = ea_data.get_value_word(pc, reg, mem, false);
-            let add_result = Cpu::sub_words(data, ea_value);
-            ea_data.set_value_word(pc, reg, mem, add_result.result, true);
-            add_result.status_register_result
+            let source = pc.fetch_next_word(mem);
+
+            let ea_data = pc.get_effective_addressing_data_from_instr_word_bit_pos(
+                instr_word,
+                reg,
+                mem,
+                |instr_word| Ok(operation_size),
+                3,
+                0,
+            )?;
+            let dest = ea_data.get_value_word(pc, reg, mem, true);
+
+            let result = Cpu::sub_words(source, dest);
+            ea_data.set_value_word(pc, reg, mem, result.result, true);
+            result.status_register_result
         }
         OperationSize::Long => {
-            let data = pc.fetch_next_long(mem);
-            let ea_value = ea_data.get_value_long(pc, reg, mem, false);
-            let add_result = Cpu::sub_longs(data, ea_value);
-            ea_data.set_value_long(pc, reg, mem, add_result.result, true);
-            add_result.status_register_result
+            let source = pc.fetch_next_long(mem);
+
+            let ea_data = pc.get_effective_addressing_data_from_instr_word_bit_pos(
+                instr_word,
+                reg,
+                mem,
+                |instr_word| Ok(operation_size),
+                3,
+                0,
+            )?;
+            let dest = ea_data.get_value_long(pc, reg, mem, true);
+
+            let result = Cpu::sub_longs(source, dest);
+            ea_data.set_value_long(pc, reg, mem, result.result, true);
+            result.status_register_result
         }
     };
 
@@ -58,44 +86,40 @@ pub fn get_disassembly<'a>(
     reg: &Register,
     mem: &Mem,
 ) -> Result<GetDisassemblyResult, GetDisassemblyResultError> {
-    let ea_data =
-        pc.fetch_effective_addressing_data_from_bit_pos_3_and_reg_pos_0(reg, mem, |instr_word| {
-            Cpu::extract_size000110_from_bit_pos_6(instr_word)
-        })?;
-    let ea_format = Cpu::get_ea_format(ea_data.ea_mode, pc, Some(ea_data.operation_size), mem);
-    match ea_data.operation_size {
+    let instr_word = pc.fetch_next_word(mem);
+    let operation_size = Cpu::extract_size000110_from_bit_pos_6(instr_word)?;
+    let immediate_data = match operation_size {
         OperationSize::Byte => {
             pc.skip_byte();
-            let data = pc.fetch_next_byte(mem);
-            Ok(GetDisassemblyResult::from_pc(
-                pc,
-                String::from("SUBI.B"),
-                format!("#${:02X},{}", data, ea_format),
-            ))
+            format!("#${:02X}", pc.fetch_next_byte(mem))
         }
-        OperationSize::Word => {
-            let data = pc.fetch_next_word(mem);
-            Ok(GetDisassemblyResult::from_pc(
-                pc,
-                String::from("SUBI.W"),
-                format!("#${:04X},{}", data, ea_format),
-            ))
-        }
-        OperationSize::Long => {
-            let data = pc.fetch_next_long(mem);
-            Ok(GetDisassemblyResult::from_pc(
-                pc,
-                String::from("SUBI.L"),
-                format!("#${:04X},{}", data, ea_format),
-            ))
-        }
-    }
+        OperationSize::Word => format!("#${:04X}", pc.fetch_next_word(mem)),
+        OperationSize::Long => format!("#${:08X}", pc.fetch_next_long(mem)),
+    };
+
+    let ea_data = pc.get_effective_addressing_data_from_instr_word_bit_pos(
+        instr_word,
+        reg,
+        mem,
+        |instr_word| Ok(operation_size),
+        3,
+        0,
+    )?;
+
+    let ea_format = Cpu::get_ea_format(ea_data.ea_mode, pc, None, mem);
+
+    Ok(GetDisassemblyResult::from_pc(
+        pc,
+        format!("SUBI.{}", ea_data.operation_size.get_format()),
+        format!("{},{}", immediate_data, ea_format),
+    ))
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
         cpu::instruction::GetDisassemblyResult,
+        mem::rammemory::RamMemory,
         register::{
             STATUS_REGISTER_MASK_CARRY, STATUS_REGISTER_MASK_EXTEND, STATUS_REGISTER_MASK_NEGATIVE,
             STATUS_REGISTER_MASK_OVERFLOW, STATUS_REGISTER_MASK_ZERO,
@@ -103,7 +127,7 @@ mod tests {
     };
 
     #[test]
-    fn subi_immediate_data_to_data_register_direct_byte() {
+    fn subi_byte_immediate_data_to_data_register_direct() {
         // arrange
         let code = [0x04, 0x07, 0x00, 0x23].to_vec(); // SUBI.B #$23,D7
         let mut cpu = crate::instr_test_setup(code, None);
@@ -138,7 +162,44 @@ mod tests {
     }
 
     #[test]
-    fn subi_immediate_data_to_data_register_direct_word() {
+    fn subi_byte_immediate_data_to_absolute_short() {
+        // arrange
+        let code = [0x04, 0x38, 0x00, 0x38, 0x60, 0x00].to_vec(); // SUBI.B #$38,($6000).W
+        let mem_range = RamMemory::from_bytes(0x00006000, [0x84].to_vec());
+        let mut mem_ranges = Vec::new();
+        mem_ranges.push(mem_range);
+        let mut cpu = crate::instr_test_setup(code, Some(mem_ranges));
+        cpu.register.reg_sr.set_sr_reg_flags_abcde(
+            STATUS_REGISTER_MASK_CARRY
+                | STATUS_REGISTER_MASK_OVERFLOW
+                | STATUS_REGISTER_MASK_ZERO
+                | STATUS_REGISTER_MASK_NEGATIVE
+                | STATUS_REGISTER_MASK_EXTEND,
+        );
+        // act assert - debug
+        let debug_result = cpu.get_next_disassembly();
+        assert_eq!(
+            GetDisassemblyResult::from_address_and_address_next(
+                0xC00000,
+                0xC00006,
+                String::from("SUBI.B"),
+                String::from("#$38,($6000).W")
+            ),
+            debug_result
+        );
+        // act
+        cpu.execute_next_instruction();
+        // assert
+        assert_eq!(0x4c, cpu.memory.get_byte(0x00006000));
+        assert_eq!(false, cpu.register.reg_sr.is_sr_carry_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_overflow_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_zero_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_negative_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_extend_set());
+    }
+
+    #[test]
+    fn subi_word_immediate_data_to_data_register_direct() {
         // arrange
         let code = [0x04, 0x47, 0x12, 0x34].to_vec(); // SUBI.W #$1234,D7
         let mut cpu = crate::instr_test_setup(code, None);
@@ -173,7 +234,44 @@ mod tests {
     }
 
     #[test]
-    fn subi_immediate_data_to_data_register_direct_long() {
+    fn subi_word_immediate_data_to_absolute_long() {
+        // arrange
+        let code = [0x04, 0x79, 0x38, 0x78, 0x00, 0x06, 0x00, 0x00].to_vec(); // SUBI.W #$3878,($60000).L
+        let mem_range = RamMemory::from_bytes(0x00060000, [0x74, 0x81].to_vec());
+        let mut mem_ranges = Vec::new();
+        mem_ranges.push(mem_range);
+        let mut cpu = crate::instr_test_setup(code, Some(mem_ranges));
+        cpu.register.reg_sr.set_sr_reg_flags_abcde(
+            STATUS_REGISTER_MASK_CARRY
+                | STATUS_REGISTER_MASK_OVERFLOW
+                | STATUS_REGISTER_MASK_ZERO
+                | STATUS_REGISTER_MASK_NEGATIVE
+                | STATUS_REGISTER_MASK_EXTEND,
+        );
+        // act assert - debug
+        let debug_result = cpu.get_next_disassembly();
+        assert_eq!(
+            GetDisassemblyResult::from_address_and_address_next(
+                0xC00000,
+                0xC00008,
+                String::from("SUBI.W"),
+                String::from("#$3878,($00060000).L")
+            ),
+            debug_result
+        );
+        // act
+        cpu.execute_next_instruction();
+        // assert
+        assert_eq!(0x3C09, cpu.memory.get_word(0x00060000));
+        assert_eq!(false, cpu.register.reg_sr.is_sr_carry_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_overflow_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_zero_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_negative_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_extend_set());
+    }
+
+    #[test]
+    fn subi_long_immediate_data_to_data_register_direct() {
         // arrange
         let code = [0x04, 0x80, 0x10, 0x10, 0x10, 0x10].to_vec(); // SUBI.L #$10101010,D0
         let mut cpu = crate::instr_test_setup(code, None);
@@ -205,5 +303,42 @@ mod tests {
         assert_eq!(false, cpu.register.reg_sr.is_sr_zero_set());
         assert_eq!(false, cpu.register.reg_sr.is_sr_negative_set());
         assert_eq!(false, cpu.register.reg_sr.is_sr_extend_set());
+    }
+
+    #[test]
+    fn subi_long_immediate_data_to_absolute_long() {
+        // arrange
+        let code = [0x04, 0xb9, 0x38, 0x78, 0x45, 0x45, 0x00, 0x04, 0x00, 0x00].to_vec(); // SUBI.L #$38784545,($00040000).L
+        let mem_range = RamMemory::from_bytes(0x00040000, [0x24, 0x81, 0x45, 0x46].to_vec());
+        let mut mem_ranges = Vec::new();
+        mem_ranges.push(mem_range);
+        let mut cpu = crate::instr_test_setup(code, Some(mem_ranges));
+        cpu.register.reg_sr.set_sr_reg_flags_abcde(
+            STATUS_REGISTER_MASK_CARRY
+                | STATUS_REGISTER_MASK_OVERFLOW
+                | STATUS_REGISTER_MASK_ZERO
+                | STATUS_REGISTER_MASK_NEGATIVE
+                | STATUS_REGISTER_MASK_EXTEND,
+        );
+        // act assert - debug
+        let debug_result = cpu.get_next_disassembly();
+        assert_eq!(
+            GetDisassemblyResult::from_address_and_address_next(
+                0xC00000,
+                0xC0000a,
+                String::from("SUBI.L"),
+                String::from("#$38784545,($00040000).L")
+            ),
+            debug_result
+        );
+        // act
+        cpu.execute_next_instruction();
+        // assert
+        assert_eq!(0xeC090001, cpu.memory.get_long(0x00040000));
+        assert_eq!(true, cpu.register.reg_sr.is_sr_carry_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_overflow_set());
+        assert_eq!(false, cpu.register.reg_sr.is_sr_zero_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_negative_set());
+        assert_eq!(true, cpu.register.reg_sr.is_sr_extend_set());
     }
 }
